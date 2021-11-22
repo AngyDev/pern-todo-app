acb3a197f32dc1c0d75901fa64aebcc2d7c58c88
Angela Busato <busato.angela@gmail.com>
2021-11-22 17:00:58 +0100

    feat: add CRUD calls to server

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import InputTodo from './components/InputTodo';
+import ListTodo from './components/ListTodo';
 
 function App() {
   return (
     <div className="container">
       <InputTodo />
+      <ListTodo />
     </div>
   );
 }
